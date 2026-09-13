622341673d7403ae9c3a979cfb6dfb858c8557b9
Amaya Senadheera <amayasenadheera6@gmail.com>
2026-09-13 09:49:46 +0530

    update greetings

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello, Amaya!" );
+        System.out.println( "Hello, Amaya Git is working!" );
 	System.out.println("I am learning Maven!");
     }
 }
